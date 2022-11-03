fff238930397e8fd00674eba6db5612ce0a48092
codetwentyfive <chingisenkhbaatar@gmail.com>
2022-11-03 15:34:30 +0100

    added playRound function

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log ("Test")
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerPlay().toLowerCase();
+    if (computerSelection === playerSelection) {
+        displayResults("TIE")
+    } else if (computerSelection = 'rock' && playerSelection= 'scissors') {
+        displayResults("YOU LOSE")
+    } else if (computerSelection = 'paper' && playerSelection= 'stone') {
+        displayResults("YOU WIN")
+    } else if (computerSelection = 'scissors' && playerSelection= 'paper') {
+        displayResults("YOU LOSE")
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
