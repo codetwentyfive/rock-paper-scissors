b89d8b5c96b57f13cdfb50aa9f389dab9f817652
codetwentyfive <chingisenkhbaatar@gmail.com>
2022-11-08 14:52:15 +0100

    reworked playRound function

--- main.js
+++ main.js
@@ -1,59 +1,40 @@
-let playerScore = 0
-let computerScore = 0
-let roundWinner = ("")
+const myArray = ['rock', 'scissors', 'paper'];
 
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
-      case 0:
-        console.log("rock")  
-        return 'rock'
-      case 1:
-        console.log("paper")  
-        return 'paper'
-      case 2:
-        console.log("scissors")  
-        return 'scissors' 
+    return myArray[Math.floor((Math.random() * myArray.length))]
+}
+
+let playerSelection
+let computerSelection
+
+function userInput() {
+    playerSelection = prompt("Whats your Choice?", '');
+    if ((playerSelection.toLocaleLowerCase() == 'rock')
+        || (playerSelection.toLocaleLowerCase() == 'paper')
+        || (playerSelection.toLocaleLowerCase() == 'scissors')) {
+        console.log(playRound(playerSelection, computerSelection));
+    } else {
+        alert("Wrong choice!");
+        userInput();
     }
+
+
 }
-
-function playerSelection () {
-    var x = document.getElementById("playerInput").value;
-    
-}
-<<<<<<< HEAD
-function playerSelection() {
-    return prompt("Whats your selection?")
-    alert (return)
-}
-=======
-
->>>>>>> 934cac598adc7e19661a47b91c06ce7da8c188a8
-
-game();
-
-computerSelection = getComputerChoice()
 
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-       return console.log ("ITSA TIE")
-    } else { return console.log("fuck me")}
-  }
+    computerSelection = getComputerChoice;
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection == playerSelection) {
+        alert("TIE");
+    } else if (
+        (computerSelection == 'rock' && playerSelection == 'scissors') ||
+        (computerSelection == 'paper' && playerSelection == 'rock') ||
+        (computerSelection == 'scissors' && playerSelection == 'paper')
+    ) {
+        alert("You lose");
+    } else (
+        alert("You Win")
+    )
 
- 
- 
-
-
-<<<<<<< HEAD
-        if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-            (playerSelection == 'paper' && computerSelection == 'rock') ||
-            (playerSelection == 'scissors' && computerSelection == 'paper')) {
-            playerScore++
-            roundWinner = 'player'
-        }
-    }
-    console.log(playerSelection);
 }
-=======
->>>>>>> 934cac598adc7e19661a47b91c06ce7da8c188a8
