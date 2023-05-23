d6d5c42fef98f6f3b09ea8bc3410172814decdc1
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-05-23 19:22:52 +0200

    fixed bug with the images

--- main.js
+++ main.js
@@ -2,17 +2,23 @@ const myArray = ['rock', 'paper', 'scissors'];
 
 const rockButton = document.getElementById("rock-btn");
 rockButton.addEventListener("click", () => {
-    playRound("rock");
+    if (!gameEnded) {
+        playRound("rock");
+    }
 });
 
 const paperButton = document.getElementById("paper-btn");
 paperButton.addEventListener("click", () => {
-    playRound("paper");
+    if (!gameEnded) {
+        playRound("paper");
+    }
 });
 
 const scissorsButton = document.getElementById("scissors-btn");
 scissorsButton.addEventListener("click", () => {
-    playRound("scissors");
+    if (!gameEnded) {
+        playRound("scissors");
+    }
 });
 
 const results = document.getElementById("results");
@@ -22,6 +28,7 @@ const replayButton = document.getElementById("replay-btn");
 
 let playerScore = 0;
 let computerScore = 0;
+let gameEnded = false;
 
 function getComputerChoice() {
     return myArray[Math.floor(Math.random() * myArray.length)];
@@ -60,17 +67,18 @@ function updateScoreboard() {
 function checkGameEnd() {
     if (playerScore >= 5 || computerScore >= 5) {
         if (playerScore > computerScore) {
-            results.textContent += "\nYou won the game!";
+            results.textContent += "\nYou won the game! Glorious!";
         } else if (playerScore < computerScore) {
-            results.textContent += "\nYou lost the game. Better luck next time!";
+            results.textContent += "\nYou lost the game youngling. Better luck next time!";
         } else {
-            results.textContent += "\nThe game ended in a tie!";
+            results.textContent += "\nTIED!";
         }
 
         // Disable the buttons
         rockButton.disabled = true;
         paperButton.disabled = true;
         scissorsButton.disabled = true;
+        gameEnded = true;
 
         // Enable the replay button
         replayButton.disabled = false;
@@ -87,11 +95,10 @@ function replayGame() {
     rockButton.disabled = false;
     paperButton.disabled = false;
     scissorsButton.disabled = false;
+    gameEnded = false;
 
     // Disable the replay button
     replayButton.disabled = true;
 }
 
 replayButton.addEventListener("click", replayGame);
-
-
