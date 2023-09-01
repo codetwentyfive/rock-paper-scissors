1b3f8953fe87291a321c61a3d2d7376426662144
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-09-02 00:56:01 +0200

    feat:optimized subpar code

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const myArray = [
 ];
 
 function computerPlay() {
-return myArray[~~(Math.random()*myArray.length)]
+  return myArray[~~(Math.random() * myArray.length)];
 }
 
 let computerSelection;
@@ -13,59 +13,67 @@ let playerSelection;
 let computerScore;
 let playerScore;
 
-game();
+const resultsDiv = document.getElementById('results');
 
-function game () {
-computerScore=0;
-playerScore=0;
-for (let i=0; i<5; i++) {
-  onUserInput();
+
+function game() {
+  computerScore = 0;
+  playerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    // We no longer need onUserInput() here since we're using buttons.
+  }
+  resultsDiv.textContent = declareWinner();
 }
-console.log(declareWinner());
 
 function declareWinner() {
-  if (playerScore===computerScore) {
-    alert(playerScore + '-' + computerScore + '\nTie game!');
+  if (playerScore === computerScore) {
     return playerScore + '-' + computerScore + '\nTie game!';
-  } else if (playerScore>computerScore) {
-    alert(playerScore + '-' + computerScore + '\nYou win!!');
+  } else if (playerScore > computerScore) {
     return playerScore + '-' + computerScore + '\nYou win!!';
   } else {
-    alert(playerScore + '-' + computerScore + '\nYou lost. Better luck next time!');
     return playerScore + '-' + computerScore + '\nYou lost. Better luck next time!';
   }
-}
-}
-
-function onUserInput() {
-playerSelection = prompt('Rock, Paper, or Scissors?',"");
-  if ((playerSelection.toLowerCase()=='rock')
-      ||(playerSelection.toLowerCase()=='paper')
-      ||(playerSelection.toLowerCase()=='scissors')) {
-    console.log(playRound(playerSelection,computerSelection));
-  } else {
-  alert('That\'s not a valid choice.');
-  onUserInput();
-}
 }
 
 function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay().toLowerCase();
   playerSelection = playerSelection.toLowerCase();
-  if (computerSelection==playerSelection) {
-      alert('Tie game!');
-      return 'Tie game\nComputer Score: ' + 
-              computerScore + '\nYour Score: ' + playerScore;
-  } else if ((computerSelection=='rock' && playerSelection=='scissors') 
-            || (computerSelection=='scissors' && playerSelection=='paper') 
-            || (computerSelection=='paper' && playerSelection=='rock')) {
-      alert('You lose! ' + computerSelection + ' beats ' + playerSelection);
-      return 'Computer Score: ' + ++computerScore + 
-              '\nYour Score: ' + playerScore;
+  let result = '';
+  
+  if (computerSelection === playerSelection) {
+    result = 'Tie game\nComputer: ' + computerSelection + '\nYou: ' + playerSelection;
+  } else if ((computerSelection == 'rock' && playerSelection == 'scissors') ||
+  (computerSelection == 'scissors' && playerSelection == 'paper') ||
+  (computerSelection == 'paper' && playerSelection == 'rock')) {
+    result = 'Computer wins\nComputer: ' + computerSelection + '\nYou: ' + playerSelection;
+    computerScore++;
   } else {
-      alert('You win! ' + playerSelection + ' beats ' + computerSelection);
-      return 'Your Score: ' + ++playerScore + 
-              '\nComputer Score: ' + computerScore;
+    result = 'You win\nComputer: ' + computerSelection + '\nYou: ' + playerSelection;
+    playerScore++;
   }
+  
+  resultsDiv.textContent = result + '\n' + declareWinner();
+   document.getElementById("playerScore").textContent='Player Score: ' +playerScore;
+   document.getElementById("computerScore").textContent='Computer Score: ' +computerScore;
 }
- 
+
+document.getElementById("replayButton").addEventListener('click', function(){
+  resultsDiv.textContent="";
+  computerScore=0;
+  playerScore=0;})
+  
+  
+  // Add event listeners for the buttons
+  document.getElementById('rockButton').addEventListener('click', function () {
+    playRound('rock', computerSelection);
+  });
+  
+  document.getElementById('paperButton').addEventListener('click', function () {
+    playRound('paper', computerSelection);
+  });
+  
+  document.getElementById('scissorsButton').addEventListener('click', function () {
+    playRound('scissors', computerSelection);
+  });
+  
+  game();
